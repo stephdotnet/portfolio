37acba5db69ea0df2d762c05a6d26a9d7a95fca8
stephdotnet <luniday@gmail.com>
2025-12-25 15:06:14 +0100

    chore: added eslint stack

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', '.astro/'],
+  },
+  eslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,mjs,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      'no-unused-vars': 'off',
+    },
+  },
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsparser,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'off',
+    },
+  },
+];
